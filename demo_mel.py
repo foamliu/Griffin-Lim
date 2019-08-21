1507c9e7099ce3ad296c2fe0002badb315ad26e5
Yang Liu <liuyang12@focusmedia.cn>
2019-08-21 16:22:01 +0800

    update

--- demo_mel.py
+++ demo_mel.py
@@ -8,6 +8,14 @@ if __name__ == '__main__':
     plt.imshow(mel, aspect='auto', origin='bottom', interpolation='none')
     plt.show()
 
+    mel -= np.min(mel)
+    mel /= np.max(mel)
+    mel = mel * 2 - 1
+    mel = mel * 4
+
+    print(np.max(mel))
+    print(np.min(mel))
+
     print("Mel spectrograms dim: ")
     print(mel.shape)
 
